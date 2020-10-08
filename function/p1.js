91d721b7e301dd29c7804af9d5a950ea0fd49968
sumit kumar <sumitsliet1830070@gmail.com>
2020-10-09 00:25:05 +0530

    Added function

---
+++ function/p1.js
@@ -0,0 +1,11 @@
+// basic syntax of a function
+function sayHello() {
+    console.log('Hello');
+}
+sayHello();
+// pass arguments to a function
+// how to use arguments with functions
+function output(text) {
+    console.log(text);
+}
+output('Hello', 'other text');
