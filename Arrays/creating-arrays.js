83a623ef9e1990d4da50603d6ff7435fe34570ca
sumit kumar <sumitsliet1830070@gmail.com>
2020-10-21 20:33:26 +0530

    Added arrays

---
+++ Arrays/creating-arrays.js
@@ -0,0 +1,20 @@
+//creating an array of length 0
+var numbers = [];
+console.log(numbers.length);
+
+// declare an array variable with a set of elements
+var numbers = [1,2,3,4,5];
+console.log(numbers.length);
+
+// create an array by calling the array constructor
+var numbers = new Array();
+console.log(numbers.length);
+
+//call the array constructor with a set of elements as arguments to the constructor
+
+var numbers = new Array(1,2,3,4,5);
+console.log(numbers.length);
+
+// call the array constructor with a single argument specifying the length of the array
+var numbers = new Array(10);
+console.log(numbers.length);
